31bd954a5041e99b9889b8fc5c6a16306780365e
Soheil <soheil.nazari@check24.de>
2025-11-18 16:03:02 +0100

    update host and port in apps

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    proxy: {
+      "/api": "0.0.0.0:3001",
+    },
+  },
+});
